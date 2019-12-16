6191f209eea1965c138bd8e4eb0956f59a4d6afa
Igor Gudich <igor.gudich@esss.se>
2019-12-16 13:53:25 +0100

    proxy settings

--- build_docker.py
+++ build_docker.py
@@ -8,9 +8,18 @@ if __name__ == "__main__":
                                                  "Mantid")
     parser.add_argument("--image_name", type=str, default="scipp_vs_mantid",
                         help="the name of the image to start")
+    parser.add_argument("--http_proxy", type=str, default="",
+                        help="use this address as http proxy.")
+    parser.add_argument("--https_proxy", type=str, default="",
+                        help="use this address as http proxy.")
 
     args = parser.parse_args()
     docker_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                "Docker")
-    command = "docker build -t {} {}".format(args.image_name, docker_path)
+    command = "docker build -t {}".format(args.image_name)
+    if args.http_proxy != "":
+        command = "{} --build-arg {}".format(command, args.http_proxy)
+    if args.https_proxy != "":
+        command = "{} --build-arg {}".format(command, args.https_proxy)
+    command = "{} {}".format(command, docker_path)
     sp.run(command, check=True, shell=True)
